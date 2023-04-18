15bb6b9b91264a339a1ec558b9b75504f67199e8
AdsonBruno <adsonbruno2@gmail.com>
2023-04-17 22:32:55 -0300

    feat: add models  ticket and ticket-response

---
+++ air-ticket-sales-api/src/models/models-response.ts
@@ -0,0 +1,6 @@
+export interface ResponseTicket {
+  flightNumber: string;
+  airline: string;
+  category: string;
+  price: number;
+}
---
+++ air-ticket-sales-api/src/models/models-ticket.ts
@@ -0,0 +1,7 @@
+export interface Ticket {
+  travelDate: string;
+  from: string;
+  to: string;
+  numberOfPassengers: number;
+  class: string;
+}
