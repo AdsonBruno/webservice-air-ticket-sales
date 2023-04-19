58e8333ddb357048d5149e4b313b06f803f8a4d5
AdsonBruno <adsonbruno2@gmail.com>
2023-04-18 23:56:50 -0300

    feat: add getFlight route

--- air-ticket-sales-api/src/index.ts
+++ air-ticket-sales-api/src/index.ts
@@ -1,9 +1,40 @@
 import express from 'express';
 import { config } from 'dotenv';
+import { PrismaGetFlightsRepository } from './repositories/get-flights/prisma-get-flights';
+import { GetFlightsController } from './controllers/get-flights/get-flights';
 
 config();
 
 const app = express();
 const port = process.env.PORT;
+
+app.get(
+  '/flights/:travelDate/:departingFrom/:arrivingAt/:numberOfPassengers/:seatType',
+  async (req, res) => {
+    const {
+      travelDate,
+      departingFrom,
+      arrivingAt,
+      numberOfPassengers,
+      seatType,
+    } = req.params;
+
+    const prismaGetFlightsRepository = new PrismaGetFlightsRepository();
+
+    const getFlightsController = new GetFlightsController(
+      prismaGetFlightsRepository
+    );
+
+    const { body, statusCode } = await getFlightsController.findFlight(
+      travelDate,
+      departingFrom,
+      arrivingAt,
+      Number(numberOfPassengers),
+      seatType
+    );
+
+    res.send(body).status(statusCode);
+  }
+);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
