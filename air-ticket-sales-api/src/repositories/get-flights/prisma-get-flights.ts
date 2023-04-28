5e0471d325fc575a9bf438e506fdd0fa01832c81
AdsonBruno <adsonbruno2@gmail.com>
2023-04-27 22:37:00 -0300

    feat: add method listFlight to fetch flights based on city

--- air-ticket-sales-api/src/repositories/get-flights/prisma-get-flights.ts
+++ air-ticket-sales-api/src/repositories/get-flights/prisma-get-flights.ts
@@ -1,70 +1,51 @@
 import { IGetFlightsRepository } from '../../controllers/get-flights/protocols';
-import { FlightSearch } from '../../models/models-ticket';
-import { HttpResponse } from '../../controllers/protocols';
 import { ResponseTicket } from '../../models/models-response';
+import { PrismaClient, Flights as PrismaFlightsModel } from '@prisma/client';
 
 export class PrismaGetFlightsRepository implements IGetFlightsRepository {
-  async listFlights(
-    travelDate: string,
-    departingFrom: string,
-    arrivingAt: string,
-    numberOfPassengers: number,
-    seatType: string
-  ): Promise<any> {
-    const fakeApi = [
-      {
-        flightNumber: 'AZU1234',
-        airline: 'Azul Linhas Aéreas',
-        category: 'econômica',
-        seatsAvailableTotal: 150,
-        seatsAvailableAtSeatType: {
-          econômica: 100,
-          executivo: 20,
-          '1a. classe': 30,
-        },
-        price: 500.0,
-        departingFrom: 'São Paulo',
-        arrivingAt: 'Rio de Janeiro',
-        travelDate: '2023-05-01',
-      },
-      {
-        flightNumber: 'GLO5678',
-        airline: 'Gol Linhas Aéreas',
-        category: 'executivo',
-        seatsAvailableTotal: 100,
-        seatsAvailableAtSeatType: {
-          econômica: 50,
-          executivo: 40,
-          '1a. classe': 10,
-        },
-        price: 1200.0,
-        departingFrom: 'Belo Horizonte',
-        arrivingAt: 'Brasília',
-        travelDate: '2023-05-02',
-      },
-      {
-        flightNumber: 'TAM9012',
-        airline: 'Latam Linhas Aéreas',
-        category: '1a. classe',
-        seatsAvailableTotal: 40,
-        seatsAvailableAtSeatType: {
-          econômica: 0,
-          executivo: 10,
-          '1a. classe': 30,
-        },
-        price: 3000.0,
-        departingFrom: 'Rio de Janeiro',
-        arrivingAt: 'São Paulo',
-        travelDate: '2023-05-03',
-      },
-    ];
+  private prisma: PrismaClient;
 
-    return fakeApi.map((flight) => ({
-      travelDate: flight.travelDate,
-      departingFrom: flight.departingFrom,
-      arrivingAt: flight.arrivingAt,
-      numberOfPassengers: 10,
-      seatType: flight.category,
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  async listFlights(
+    departingFrom: string,
+    arrivingAt: string
+  ): Promise<ResponseTicket[]> {
+    const flights: PrismaFlightsModel[] = await this.prisma.flights.findMany({
+      where: {
+        origin: departingFrom,
+        destination: arrivingAt,
+      },
+    });
+
+    console.log(flights);
+
+    return flights.map((flight: PrismaFlightsModel) => ({
+      flightNumber: flight.flight_id,
+      airline: flight.airline,
+      origin: flight.origin,
+      destination: flight.destination,
+      departureTime: flight.departure_time,
+      arrivalTime: flight.arrival_time,
+      price: 1500,
+      category: '1ª classe',
+    }));
+  }
+
+  async listAllFlights(): Promise<ResponseTicket[]> {
+    const flights = await this.prisma.flights.findMany();
+
+    return flights.map((flights: PrismaFlightsModel) => ({
+      flightNumber: flights.flight_id,
+      airline: flights.airline,
+      origin: flights.origin,
+      destination: flights.destination,
+      departureTime: flights.departure_time,
+      arrivalTime: flights.arrival_time,
+      category: 'Econômica',
+      price: 150,
     }));
   }
 }
