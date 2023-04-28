6230731441e071df6d0232c23b685b254275ae5a
AdsonBruno <adsonbruno2@gmail.com>
2023-04-28 10:49:20 -0300

    feat: add methods to perform API queries

--- front-end/public/script.js
+++ front-end/public/script.js
@@ -1,28 +1,31 @@
-const { default: axios } = require('axios');
 
 console.log('Hello Word');
 
-async function getFlights() {
-  const response = await fetch('/flights');
-  const data = await response.json();
 
+
+async function getFlights() {
+  const response = await axios.get('http://localhost:5000/flights');
+  const data = await response.data;
+  console.log('Passou aqui');
   console.log(data);
+
 }
 
 async function getFlightsOrigin() {
-  const response = await axios('/flights/:origin');
+  const response = await axios.get('/flights/:origin');
   const data = await response.json();
 
   console.log(data)
 }
 
 async function getFlightsOriginAndDestination() {
-  const response = await axios('/flights/:origin/:destination');
+  const response = await axios.get('/flights/:origin/:destination');
   const data = await response.json();
 
   console.log(data)
 }
 
-getFlights();
-getFlightsOrigin();
-getFlightsOriginAndDestination();
+const btnSearch = document.querySelector('#btnSearch');
+btnSearch.addEventListener("click", () => {
+  getFlights();
+});
