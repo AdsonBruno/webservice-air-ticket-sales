9b8c6e054092275a29c981551370d94df47c0166
AdsonBruno <adsonbruno2@gmail.com>
2023-05-02 17:27:56 -0300

    feat: add access to endpoints to be able to access the database and show routes

--- front-end/public/script.js
+++ front-end/public/script.js
@@ -5,9 +5,23 @@ console.log('Hello Word');
 
 async function getFlights() {
   const response = await axios.get('http://localhost:5000/flights');
-  const data = await response.data;
+  const flights = response.data;
   console.log('Passou aqui');
-  console.log(data);
+
+  const flightList = document.getElementById('flightList');
+
+  while (flightList.firstChild) {
+    flightList.removeChild(flightList.firstChild);
+  }
+
+  flights.forEach((flight) => {
+    const li = document.createElement('li');
+    li.textContent = `Flight from ${flight.origin} to ${flight.destination} at ${flight.departureTime}`;
+
+    flightList.appendChild(li);
+  })
+
+  // console.log(data);
 
 }
 
@@ -18,14 +32,40 @@ async function getFlightsOrigin() {
   console.log(data)
 }
 
-async function getFlightsOriginAndDestination() {
-  const response = await axios.get('/flights/:origin/:destination');
-  const data = await response.json();
+async function getFlightsOriginAndDestination(origin, destination) {
+  const response = await axios.get(`/flights/${origin}/${destination}`);
+  console.log(response)
 
-  console.log(data)
+  const flights = await response.data;
+
+  const flightList = document.getElementById('flightList');
+
+  while (flightList.firstChild) {
+    flightList.removeChild(flightList.firstChild);
+  }
+
+  flights.forEach((flight) => {
+    const li = document.createElement('li');
+    li.textContent = `Flight De ${flight.origin} Para ${flight.destination} Horário de embarque ${flight.departureTime} Empresa ${flight.airline}`;
+
+    flightList.appendChild(li);
+  })
+  console.log(flights)
 }
 
 const btnSearch = document.querySelector('#btnSearch');
 btnSearch.addEventListener("click", () => {
+  const origin = document.querySelector('#origin').value;
+  const destination = document.querySelector('#destination').value;
+
+  getFlightsOriginAndDestination(origin, destination);
+
+});
+
+
+const btnSearchAll = document.querySelector('#btnSearchAll');
+btnSearchAll.addEventListener("click", () => {
+
   getFlights();
+
 });
